70c35a20fd64e138cbd374adfa0926a470bd71e2
boris-becomes <boris.bosnjak@becomes.co>
2020-10-15 09:36:00 +0200

    Add sidenav lvl2 component

--- src/components/layout/manager.svelte
+++ src/components/layout/manager.svelte
@@ -1,43 +1,21 @@
 <script lang="ts">
-  import { createEventDispatcher } from 'svelte';
-  import { fade } from 'svelte/transition';
-  import Button from '../button.svelte';
-  import Link from '../link.svelte';
+  import SideNavLvl2 from './side-nav-lvl2.svelte';
+  import { fly } from 'svelte/transition';
 
   type Item = {
     name: string;
     link: string;
     selected: boolean;
-  }
+  };
 
   export let label = '';
   export let items: Item[] = [];
   export let actionText = '';
-
-  const dispatch = createEventDispatcher();
 </script>
 
-<div
-  class="manager-layout">
-  <div class="manager-layout--side-menu">
-    <h3>{label}</h3>
-    <div class="items">
-      {#each items as item}
-        <Link class="item {item.selected ? 'selected' : ''}" href={item.link}>
-          <div class="icon fas fa-arrow-right" />
-          <div class="name">{item.name}</div>
-        </Link>
-      {/each}
-    </div>
-    <Button
-      class="mt--auto"
-      kind="ghost"
-      icon="fas fa-plus"
-      on:click={() => {
-        dispatch('action');
-      }}>
-      {actionText}
-    </Button>
+<div class="manager-layout">
+  <div in:fly={{ delay: 500, duration: 500, x: -50 }}>
+    <SideNavLvl2 {label} {items} {actionText} on:action />
   </div>
   <div class="manager-layout--content">
     <div class="manager-layout--content-wrapper">
