677de8ea7f1038f87e952fcad4abf82ee2f21be7
boris-becomes <boris.bosnjak@becomes.co>
2020-10-19 16:29:30 +0200

    EntityInfo component

--- src/components/entity-info.svelte
+++ src/components/entity-info.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { createEventDispatcher } from 'svelte';
   import Button from './button.svelte';
+  import { DateUtil } from '../util';
   import MarkdownBoxDisplay from './markdown-box-display.svelte';
 
   export let id: string;
@@ -9,16 +10,17 @@
   export let name: string;
   export let description: string;
   export let whereIsItUsed: boolean = false;
+  export let singleEntry: boolean;
 
   const dispatch = createEventDispatcher();
 </script>
 
-<div class="entity-info">
-  <div class="entity-info--left">
-    <div class="name">
-      <h3>{name}</h3>
+<div class="entityInfo entityInfo--cols">
+  <div class="entityInfo--col entityInfo--col_left">
+    <div class="entityInfo--heading">
+      <h2 class="entityInfo--title">{name}</h2>
       <Button
-        class="ml--10"
+        class="entityInfo--heading-rename ml--10"
         kind="ghost"
         icon="fas fa-edit"
         size="s"
@@ -51,19 +53,36 @@
         markdown={description}
         fallbackText="This entity does not have a description." />
     {/if}
+    <p class="entityInfo--description mb--60">
+      {description ? description : 'No description provided'}
+    </p>
+    <p class="bcmsInput--label">Entry type</p>
+    <label class="checkboxLabel">
+      <input
+        type="checkbox"
+        checked={singleEntry}
+        on:change={(event) => {
+          dispatch('editEntryType', event.target.checked);
+        }} />
+      <span class="checkboxLabel-textContent ml--10">Single</span>
+    </label>
   </div>
-  <div class="entity-info--right">
-    <div class="stat">
-      <label for="id">ID</label>
-      <p>{id}</p>
-    </div>
-    <div class="stat">
-      <label for="created-at">Created at</label>
-      <p>{new Date(createdAt).toLocaleString()}</p>
-    </div>
-    <div class="stat">
-      <label for="updated-at">Updated at</label>
-      <p>{new Date(updatedAt).toLocaleString()}</p>
-    </div>
+  <div class="entityInfo--col entityInfo--col_right">
+    <p class="entityInfo--basicInfo">
+      <span class="entityInfo--basicInfo-title mb--10">ID</span>
+      <span class="entityInfo--basicInfo-value">{id}</span>
+    </p>
+    <p class="entityInfo--basicInfo">
+      <span class="entityInfo--basicInfo-title mb--10">Created at</span>
+      <span
+        class="entityInfo--basicInfo-value"
+        title={DateUtil.readableDate(createdAt).dateFormat}>{DateUtil.readableDate(createdAt).dateFormat}</span>
+    </p>
+    <p class="entityInfo--basicInfo">
+      <span class="entityInfo--basicInfo-title mb--10">Updated at</span>
+      <span
+        class="entityInfo--basicInfo-value"
+        title={DateUtil.readableDate(updatedAt).dateFormat}>{DateUtil.readableDate(updatedAt).dateFormat}</span>
+    </p>
   </div>
 </div>
