341642e98e59120835883e46b8ef73e6a63d9d42
boris-becomes <boris.bosnjak@becomes.co>
2020-10-19 17:08:21 +0200

    Use inline svgs

--- src/components/entity-editor.svelte
+++ src/components/entity-editor.svelte
@@ -62,7 +62,38 @@
         <li class="entityEditor--list-item entityEditor--cols">
           <div class="entityEditor--list-name">
             <span>
-              <i class="fas fa-{prop.required ? 'lock' : 'unlock'}" />
+              {#if prop.required}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24">
+                  <path
+                    fill="#000"
+                    fill-rule="evenodd"
+                    d="M5 12a1 1 0 00-1 1v7a1 1 0 001 1h14a1 1 0 001-1v-7a1 1 0 00-1-1H5zm-3 1a3 3 0 013-3h14a3 3 0 013 3v7a3 3 0 01-3 3H5a3 3 0 01-3-3v-7z"
+                    clip-rule="evenodd" />
+                  <path
+                    fill="#000"
+                    fill-rule="evenodd"
+                    d="M12 3a4 4 0 00-4 4v4a1 1 0 11-2 0V7a6 6 0 1112 0v4a1 1 0 11-2 0V7a4 4 0 00-4-4z"
+                    clip-rule="evenodd" />
+                </svg>
+              {:else}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24">
+                  <path
+                    fill="#000"
+                    fill-rule="evenodd"
+                    d="M5 12a1 1 0 00-1 1v7a1 1 0 001 1h14a1 1 0 001-1v-7a1 1 0 00-1-1H5zm-3 1a3 3 0 013-3h14a3 3 0 013 3v7a3 3 0 01-3 3H5a3 3 0 01-3-3v-7z"
+                    clip-rule="evenodd" />
+                  <path
+                    fill="#000"
+                    d="M9.172 4.172A4 4 0 0116 7h2A6 6 0 106 7v4a1 1 0 102 0V7a4 4 0 011.172-2.828z" />
+                  <path fill="#000" d="M18 7a1 1 0 11-2 0 1 1 0 012 0z" />
+                </svg>
+              {/if}
             </span>
             <span>{prop.label}</span>
           </div>
