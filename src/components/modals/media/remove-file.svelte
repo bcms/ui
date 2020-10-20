d8000324d5098d3f72fa1c7b4c65c745efdec1c6
boris-becomes <boris.bosnjak@becomes.co>
2020-10-20 17:18:12 +0200

    Add remove file modal component

---
+++ src/components/modals/media/remove-file.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+  import { StoreService } from '../../../services';
+  import Modal from '../modal.svelte';
+
+  const dispatch = createEventDispatcher();
+  const modalName = 'MediaRemoveFileModal';
+
+  function close() {
+    StoreService.update(modalName, false);
+  }
+
+  function cancel() {
+    dispatch('cancel');
+    close();
+  }
+</script>
+
+<Modal
+  title="Delete File"
+  name={modalName}
+  on:cancel={cancel}
+  on:done={() => {
+    dispatch('done');
+  }} />
