4b496d9bccc8b4b85c2b9343c17c3b52dbe6d351
boris-becomes <boris.bosnjak@becomes.co>
2020-11-25 11:17:00 +0100

    SDK

--- src/views/entry/editor.svelte
+++ src/views/entry/editor.svelte
@@ -32,7 +32,6 @@
     EntryAddContentSectionModal,
   } from '../../components';
   import { EntryUtil } from '../../util';
-  import type { stringify } from 'querystring';
   export let templateId: string;
   export let entryId: string;
   type ErrorObject = {
