abc975afb16b73995c8debf24ac0041577a28ec7
boris-becomes <boris.bosnjak@becomes.co>
2020-11-30 10:42:39 +0100

    Add missing modals

--- src/components/modals/index.ts
+++ src/components/modals/index.ts
@@ -1,4 +1,4 @@
-export * from './media'
+export * from './media';
 export * from './entry';
 export { default as Modal } from './modal.svelte';
 export { default as AddPropModal } from './add-prop.svelte';
@@ -7,3 +7,5 @@ export { default as EditPropModal } from './edit-prop.svelte';
 export { default as EditUserModal } from './edit-user.svelte';
 export { default as AddUserModal } from './add-user.svelte';
 export { default as WhereIsItUsedModal } from './where-is-it-used.svelte';
+export { default as MakeAnAdminModal } from './make-an-admin.svelte';
+export { default as ConfirmDeleteModal } from './confirm-delete.svelte';
