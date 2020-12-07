89f9d637882cc44b8234df94ce8fd503ef8a9fd4
boris-becomes <boris.bosnjak@becomes.co>
2020-12-07 12:33:43 +0100

    Check if there are items in WhereIsItUsedModal

--- src/components/modals/where-is-it-used.svelte
+++ src/components/modals/where-is-it-used.svelte
@@ -32,81 +32,85 @@
   <div slot="header">
     <h2 class="bcmsModal--title">{title}</h2>
   </div>
-  <ul class="bcmsModal_whereIsItUsed--list">
-    <li
-      class="bcmsModal_whereIsItUsed--list-item bcmsModal_whereIsItUsed--list-cols">
-      <div
-        class="bcmsModal_whereIsItUsed--list-item-type bcmsModal_whereIsItUsed--list-item-col"
-        data-column-name="Type">
-        Type
-      </div>
-      <div
-        class="bcmsModal_whereIsItUsed--list-item-label bcmsModal_whereIsItUsed--list-item-col"
-        data-column-name="Label">
-        Label
-      </div>
-      <div
-        class="bcmsModal_whereIsItUsed--list-item-location bcmsModal_whereIsItUsed--list-item-col"
-        data-column-name="Location">
-        Location
-      </div>
-    </li>
-    {#each items as item}
+  {#if items.length > 0}
+    <ul class="bcmsModal_whereIsItUsed--list">
       <li
         class="bcmsModal_whereIsItUsed--list-item bcmsModal_whereIsItUsed--list-cols">
         <div
           class="bcmsModal_whereIsItUsed--list-item-type bcmsModal_whereIsItUsed--list-item-col"
           data-column-name="Type">
-          {GeneralService.string.toPretty(item.type)}
+          Type
         </div>
         <div
           class="bcmsModal_whereIsItUsed--list-item-label bcmsModal_whereIsItUsed--list-item-col"
           data-column-name="Label">
-          {#if item.template}<span>{item.template.label}</span>{/if}
-          {item.label}
+          Label
         </div>
         <div
           class="bcmsModal_whereIsItUsed--list-item-location bcmsModal_whereIsItUsed--list-item-col"
           data-column-name="Location">
-          {#if item.type === 'entry'}
-            <Link
-              on:click={() => {
-                close();
-              }}
-              href="/dashboard/template/{item.template.id}/entry/{item.id}">
-              <span>Open entry</span>
-              <LinkIcon />
-            </Link>
-          {:else if item.type === 'widget'}
-            <Link
-              on:click={() => {
-                close();
-              }}
-              href="/dashboard/widget/editor/{item.id}">
-              <span>Open widget</span>
-              <LinkIcon />
-            </Link>
-          {:else if item.type === 'group'}
-            <Link
-              on:click={() => {
-                close();
-              }}
-              href="/dashboard/group/editor/{item.id}">
-              <span>Open group</span>
-              <LinkIcon />
-            </Link>
-          {:else if item.type === 'template'}
-            <Link
-              on:click={() => {
-                close();
-              }}
-              href="/dashboard/template/editor/{item.id}">
-              <span>Open group</span>
-              <LinkIcon />
-            </Link>
-          {/if}
+          Location
         </div>
       </li>
-    {/each}
-  </ul>
+      {#each items as item}
+        <li
+          class="bcmsModal_whereIsItUsed--list-item bcmsModal_whereIsItUsed--list-cols">
+          <div
+            class="bcmsModal_whereIsItUsed--list-item-type bcmsModal_whereIsItUsed--list-item-col"
+            data-column-name="Type">
+            {GeneralService.string.toPretty(item.type)}
+          </div>
+          <div
+            class="bcmsModal_whereIsItUsed--list-item-label bcmsModal_whereIsItUsed--list-item-col"
+            data-column-name="Label">
+            {#if item.template}<span>{item.template.label}</span>{/if}
+            {item.label}
+          </div>
+          <div
+            class="bcmsModal_whereIsItUsed--list-item-location bcmsModal_whereIsItUsed--list-item-col"
+            data-column-name="Location">
+            {#if item.type === 'entry'}
+              <Link
+                on:click={() => {
+                  close();
+                }}
+                href="/dashboard/template/{item.template.id}/entry/{item.id}">
+                <span>Open entry</span>
+                <LinkIcon />
+              </Link>
+            {:else if item.type === 'widget'}
+              <Link
+                on:click={() => {
+                  close();
+                }}
+                href="/dashboard/widget/editor/{item.id}">
+                <span>Open widget</span>
+                <LinkIcon />
+              </Link>
+            {:else if item.type === 'group'}
+              <Link
+                on:click={() => {
+                  close();
+                }}
+                href="/dashboard/group/editor/{item.id}">
+                <span>Open group</span>
+                <LinkIcon />
+              </Link>
+            {:else if item.type === 'template'}
+              <Link
+                on:click={() => {
+                  close();
+                }}
+                href="/dashboard/template/editor/{item.id}">
+                <span>Open group</span>
+                <LinkIcon />
+              </Link>
+            {/if}
+          </div>
+        </li>
+      {/each}
+    </ul>
+  {:else}
+    <div class="managerPropsEditor--empty">It hasn't been used yet</div>
+  {/if}
 </Modal>
