798eec081e931044ffd4455aa03bb0766b5448ab
Momcilo Popov <popovmomcilo@gmail.com>
2020-12-10 17:42:17 +0100

    Background size small fix

--- src/components/glow.svelte
+++ src/components/glow.svelte
@@ -4,7 +4,7 @@
 <div class="bcmsGlow">
   <div class="bcmsGlow--inner">
     <img data-bcms-glow="yellow-green" src="/assets/gradients/yellow-green.jpg" style="width:39.93vw" alt="" />
-    <img data-bcms-glow="pink-yellow" src="/assets/gradients/pink-yellow.jpg" style="width:43.75" alt="" />
+    <img data-bcms-glow="pink-yellow" src="/assets/gradients/pink-yellow.jpg" style="width:43.75vw" alt="" />
     <img data-bcms-glow="pink-yellow-green" src="/assets/gradients/pink-yellow-green.jpg" style="width:39.51vw" alt="" />
     <img data-bcms-glow="pink-green" src="/assets/gradients/pink-green.jpg" style="width:49.23vw" alt="" />
     <img data-bcms-glow="green-yellow" src="/assets/gradients/green-yellow.jpg" style="width:43.19vw" alt="" />
