0d42137651413eae56cf2f6974dcf7a496c040cb
Bbanez <banezprod@gmail.com>
2020-12-11 15:48:07 +0100

    Update 404

--- src/views/p404.svelte
+++ src/views/p404.svelte
@@ -1,6 +1,22 @@
 <script lang="ts">
-  import { Link } from '../components';
+  import { Meta } from '../components';
+  import { LogoIcon } from '../components/icons';
 </script>
 
-<h1>This page does not exist.</h1>
-<Link href="/">Go to login</Link>
+<style>
+  h1 {
+    color: rgba(var(--grey), 1);
+    font-size: 100px;
+    text-align: center;
+  }
+</style>
+
+<Meta title="Not found" />
+<div class="auth login">
+  <div class="auth--content">
+    <div class="auth--bcmsLogo">
+      <LogoIcon />
+    </div>
+    <h1>404</h1>
+  </div>
+</div>
