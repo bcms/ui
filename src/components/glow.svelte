596eada39d20fc40771de735e6935b388f7ebb9c
Momcilo Popov <popovmomcilo@gmail.com>
2020-12-16 11:18:27 +0100

    Update glow images and their sizes

--- src/components/glow.svelte
+++ src/components/glow.svelte
@@ -3,10 +3,10 @@
 
 <div class="bcmsGlow">
   <div class="bcmsGlow--inner">
-    <img data-bcms-glow="yellow-green" src="/assets/gradients/yellow-green.jpg" style="width:39.93vw" alt="" />
-    <img data-bcms-glow="pink-yellow" src="/assets/gradients/pink-yellow.jpg" style="width:43.75vw" alt="" />
-    <img data-bcms-glow="pink-yellow-green" src="/assets/gradients/pink-yellow-green.jpg" style="width:39.51vw" alt="" />
-    <img data-bcms-glow="pink-green" src="/assets/gradients/pink-green.jpg" style="width:49.23vw" alt="" />
-    <img data-bcms-glow="green-yellow" src="/assets/gradients/green-yellow.jpg" style="width:43.19vw" alt="" />
+    <img data-bcms-glow="yellow-green" loading="lazy" src="/assets/gradients/yellow-green.png" alt="" />
+    <img data-bcms-glow="pink-yellow" loading="lazy" src="/assets/gradients/pink-yellow.png" alt="" />
+    <img data-bcms-glow="pink-yellow-green" loading="lazy" src="/assets/gradients/pink-yellow-green.png" alt="" />
+    <img data-bcms-glow="pink-green" loading="lazy" src="/assets/gradients/pink-green.png" alt="" />
+    <img data-bcms-glow="green-yellow" loading="lazy" src="/assets/gradients/green-yellow.png" alt="" />
   </div>
 </div>
