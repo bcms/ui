010f3d175a0c9295d66e8295e10e8cb13819a1c1
boris-becomes <boris.bosnjak@becomes.co>
2021-03-03 16:29:40 +0100

    Add placeholder for non-image files

--- src/components/props/media/inner.svelte
+++ src/components/props/media/inner.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { createEventDispatcher } from 'svelte';
-  import { TrashIcon } from '../../icons';
+  import { FileIcon, TrashIcon } from '../../icons';
   import Image from '../../image.svelte';
 
   export { className as class };
@@ -9,15 +9,26 @@
 
   const dispatch = createEventDispatcher();
   let className = '';
+
+  function isFileImage(src: string): boolean {
+    return /\.(gif|jpe?g|tiff?|png|webp|bmp|svg)$/i.test(src);
+  }
 </script>
 
 <div
-  class="bcmsMedia--inner {invalidText && !value ? 'bcmsMedia--inner_isError' : ''}
-    {className}">
+  class="bcmsMedia--inner {invalidText && !value
+    ? 'bcmsMedia--inner_isError'
+    : ''}
+    {className}"
+>
   {#if value !== ''}
     <button on:click class="bcmsMedia--details">
       <div class="bcmsMedia--details-visual">
-        <Image href={value} data-src={value} alt="" />
+        {#if isFileImage(value)}
+          <Image href={value} data-src={value} alt="" />
+          {:else}
+          <FileIcon />
+        {/if}
       </div>
       <div class="bcmsMedia--details-info">
         <div class="bcmsMedia--path">{value}</div>
@@ -29,13 +40,16 @@
       class="bcmsMedia--actions"
       on:click={() => {
         dispatch('clear');
-      }}>
+      }}
+    >
       <TrashIcon />
     </button>
   {:else}
     <button on:click class="bcmsMedia--details">
       <div class="bcmsMedia--details-cta">
-        {invalidText ? 'Media file is required. Please select one' : 'Click to select a media'}
+        {invalidText
+          ? 'Media file is required. Please select one'
+          : 'Click to select a media'}
       </div>
     </button>
   {/if}
