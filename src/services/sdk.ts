9265512eaa5656523c9d3e8076b1152f79d57bba
Bbanez <banezprod@gmail.com>
2021-05-26 18:37:24 +0200

    Update and audit packages

--- src/services/sdk.ts
+++ src/services/sdk.ts
@@ -1,11 +1,8 @@
 import { BCMS } from '@becomes/cms-sdk';
 
-window.dev = '__DEV__';
-window.dev = window.dev === 'true' ? true : false;
-
 export const sdk = BCMS({
   cms: {
-    origin: window.dev ? 'http://localhost:1280' : '',
+    origin: '',
   },
   storage: {
     prfx: 'bcms',
