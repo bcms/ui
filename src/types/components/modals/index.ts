8dc6bbace23bb287547d712ce83cf19c62aca82b
boris-becomes <boris.bosnjak@becomes.co>
2021-06-16 12:31:01 +0200

    Add ShowDescriptionExample modal

--- src/types/components/modals/index.ts
+++ src/types/components/modals/index.ts
@@ -7,3 +7,4 @@ export * from './add-update-template';
 export * from './add-update-widget';
 export * from './confirm';
 export * from './where-is-it-used';
+export * from './show-description-example';
