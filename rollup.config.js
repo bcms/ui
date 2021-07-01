0a40b0bced691d9e40bd4f0517d57def2084cfe9
Bbanez <banezprod@gmail.com>
2021-07-01 14:32:43 +0200

    Add json rollup plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import svelte from 'rollup-plugin-svelte';
+import json from '@rollup/plugin-json';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
@@ -87,6 +88,9 @@ export default {
       dedupe: ['svelte'],
     }),
     commonjs(),
+    json({
+      compact: true
+    }),
     typescript({
       sourceMap: !production,
       inlineSources: !production,
