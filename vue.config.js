915a505ef2e0dad159e874a6c18d51df44e35a81
boris-becomes <boris.bosnjak@becomes.co>
2021-10-08 12:54:39 +0200

    Moving editor styles to Tailwind

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,9 @@ module.exports = {
   css: {
     loaderOptions: {
       sass: {
-        prependData: `@import "~@/assets/styles/settings/mixins.scss";`,
+        prependData: `@import "~@/assets/styles/settings/mixins.scss";
+        @import "~@/assets/styles/settings/variables.scss";
+        @import "~@/assets/styles/settings/z-index.scss";`,
       },
     },
   },
