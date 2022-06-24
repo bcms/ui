825d23a28dd9b1ddf02672f780cb2d9ca0f4e33d
Bbanez <banezprod@gmail.com>
2022-06-24 14:03:20 +0200

    Improve select dropdown

--- src/components/input/select/list.vue
+++ src/components/input/select/list.vue
@@ -1,10 +1,20 @@
 <script lang="tsx">
-import { defineComponent, PropType } from 'vue';
+import {
+  CSSProperties,
+  defineComponent,
+  onMounted,
+  onUnmounted,
+  PropType,
+  ref,
+  Ref,
+  Teleport,
+} from 'vue';
 import * as uuid from 'uuid';
 import type { BCMSSelectOption } from '../../../types';
 
 const component = defineComponent({
   props: {
+    inputRef: Object as PropType<Ref<HTMLElement | null>>,
     options: {
       type: Array as PropType<BCMSSelectOption[]>,
       required: true,
@@ -29,9 +39,18 @@ const component = defineComponent({
     change: (_option: BCMSSelectOption) => {
       return true;
     },
+    hide: () => {
+      return true;
+    },
   },
   setup(props, ctx) {
     const scrollerId = uuid.v4();
+    const container = ref<HTMLElement>();
+    const positionStyles = ref<CSSProperties>({
+      top: '0 !important',
+      left: '0 !important',
+      width: '100px !important',
+    });
 
     const logic = {
       isItemSelected(option: BCMSSelectOption) {
@@ -44,51 +63,122 @@ const component = defineComponent({
           ctx.emit('change', option);
         }
       },
+      updatePosition() {
+        if (props.inputRef) {
+          const parentEl = props.inputRef.value;
+          if (parentEl && container.value) {
+            const bb = parentEl.getBoundingClientRect();
+            console.log(parentEl);
+            if (
+              bb.bottom + 10 + container.value.offsetHeight >
+              window.innerHeight
+            ) {
+              positionStyles.value.top = `${
+                bb.top - 10 - container.value.offsetHeight
+              }px !important`;
+            } else {
+              positionStyles.value.top = `${bb.bottom + 10}px !important`;
+            }
+            positionStyles.value.width = `${parentEl.clientWidth}px !important`;
+            positionStyles.value.left = `${bb.left}px !important`;
+          }
+        }
+      },
+      findScrollableEls(from: HTMLElement) {
+        if (from.scrollHeight > from.offsetHeight) {
+          // console.log(from.scrollHeight, from.offsetHeight, from);
+          scrollableParents.push(from);
+        }
+        if (from.parentElement) {
+          logic.findScrollableEls(from.parentElement);
+        }
+      },
     };
+    logic.updatePosition();
+    const scrollableParents: HTMLElement[] = [];
+    const obs = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (!entry.isIntersecting) {
+            ctx.emit('hide');
+          }
+        });
+      },
+      {
+        threshold: 1,
+      }
+    );
+
+    onMounted(() => {
+      if (props.inputRef && props.inputRef.value) {
+        obs.observe(props.inputRef.value);
+
+        logic.findScrollableEls(props.inputRef.value);
+        for (let i = 0; i < scrollableParents.length; i++) {
+          const element = scrollableParents[i];
+          element.addEventListener('scroll', logic.updatePosition);
+        }
+      }
+      logic.updatePosition();
+    });
+
+    onUnmounted(() => {
+      if (props.inputRef && props.inputRef.value) {
+        obs.unobserve(props.inputRef.value);
+      }
+      for (let i = 0; i < scrollableParents.length; i++) {
+        const element = scrollableParents[i];
+        element.removeEventListener('scroll', logic.updatePosition);
+      }
+    });
 
     return () => {
       return (
-        <ul
-          id={scrollerId}
-          tabindex="-1"
-          role="listbox"
-          aria-labelledby="bcmsSelect_label"
-          class={`max-h-[200px] min-w-[150px] list-none w-full bg-white border border-grey border-opacity-20 z-100 rounded-2.5 transition-shadow duration-300 left-0 overflow-auto ${
-            props.showSearch ? 'border-none pt-6' : 'absolute shadow-cardLg'
-          } ${props.invalidText ? 'border-red' : ''} bcmsScrollbar`}
-        >
-          {props.options.map((option) => (
-            <li
-              role="option"
-              tabindex="0"
-              class={`py-2.5 before:block relative cursor-pointer text-dark transition-colors duration-200 flex items-center hover:bg-light focus:bg-light focus:outline-none ${
-                logic.isItemSelected(option)
-                  ? 'selected before:w-2.5 before:h-2.5 before:bg-yellow before:absolute before:rounded-full before:top-1/2 before:left-[-5px] before:-translate-y-1/2 hover:before:bg-red focus:before:bg-red'
-                  : 'hover:before:w-2.5 hover:before:h-2.5 hover:before:bg-yellow hover:before:absolute hover:before:rounded-full hover:before:top-1/2 hover:before:left-[-5px] hover:before:-translate-y-1/2 focus:before:w-2.5 focus:before:h-2.5 focus:before:bg-yellow focus:before:absolute focus:before:rounded-full focus:before:top-1/2 focus:before:left-[-5px] focus:before:-translate-y-1/2'
-              } ${props.showSearch ? 'pl-1 pr-4.5 before:hidden' : 'px-4.5'}`}
-              data-value={option.value}
-              onKeydown={(event) => {
-                if (event.key === 'Enter') {
+        <Teleport to="#selectList">
+          <ul
+            id={scrollerId}
+            ref={container}
+            tabindex="-1"
+            role="listbox"
+            aria-labelledby="bcmsSelect_label"
+            class={`fixed max-h-[200px] min-w-[150px] list-none w-full bg-white border border-grey border-opacity-20 z-[1000000] rounded-2.5 transition-shadow duration-300 left-0 overflow-auto shadow-cardLg ${
+              props.showSearch ? 'border-none pt-6' : ''
+            } ${props.invalidText ? 'border-red' : ''} bcmsScrollbar`}
+            style={positionStyles.value}
+          >
+            {props.options.map((option) => (
+              <li
+                role="option"
+                tabindex="0"
+                class={`py-2.5 before:block relative cursor-pointer text-dark transition-colors duration-200 flex items-center hover:bg-light focus:bg-light focus:outline-none ${
+                  logic.isItemSelected(option)
+                    ? 'selected before:w-2.5 before:h-2.5 before:bg-yellow before:absolute before:rounded-full before:top-1/2 before:left-[-5px] before:-translate-y-1/2 hover:before:bg-red focus:before:bg-red'
+                    : 'hover:before:w-2.5 hover:before:h-2.5 hover:before:bg-yellow hover:before:absolute hover:before:rounded-full hover:before:top-1/2 hover:before:left-[-5px] hover:before:-translate-y-1/2 focus:before:w-2.5 focus:before:h-2.5 focus:before:bg-yellow focus:before:absolute focus:before:rounded-full focus:before:top-1/2 focus:before:left-[-5px] focus:before:-translate-y-1/2'
+                } ${props.showSearch ? 'pl-1 pr-4.5 before:hidden' : 'px-4.5'}`}
+                data-value={option.value}
+                onKeydown={(event) => {
+                  if (event.key === 'Enter') {
+                    logic.selectOption(option);
+                  }
+                }}
+                onClick={() => {
                   logic.selectOption(option);
-                }
-              }}
-              onClick={() => {
-                logic.selectOption(option);
-              }}
-            >
-              {option.image ? (
-                <img
-                  class="w-6 h-6 rounded-full mr-[15px]"
-                  src={option.image}
-                  alt="Flag"
-                />
-              ) : (
-                ''
-              )}
-              <span>{option.label ? option.label : option.value}</span>
-            </li>
-          ))}
-        </ul>
+                }}
+              >
+                {option.image ? (
+                  <img
+                    class="w-6 h-6 rounded-full mr-[15px]"
+                    src={option.image}
+                    alt="Flag"
+                  />
+                ) : (
+                  ''
+                )}
+                <span>{option.label ? option.label : option.value}</span>
+              </li>
+            ))}
+          </ul>
+        </Teleport>
       );
     };
   },
