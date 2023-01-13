904946752e063e3a5330ee7733417d624cec62dc
Bbanez <banezprod@gmail.com>
2023-01-13 15:24:48 +0100

    Fix JSON parse error

--- src/services/prop.ts
+++ src/services/prop.ts
@@ -413,13 +413,9 @@ export function createBcmsPropService(): void {
             );
             if (propValue) {
               propValue.push(
-                JSON.parse(
-                  JSON.stringify(
-                    value.data instanceof Array
-                      ? (value.data as string[])[0]
-                      : (value.data as BCMSPropValueGroupPointerData).items[0]
-                  )
-                )
+                value.data instanceof Array
+                  ? (value.data as string[])[0]
+                  : (value.data as BCMSPropValueGroupPointerData).items[0]
               );
             }
           }
