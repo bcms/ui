d39745bf6d894bcb253bc5b0281cc9887f2c3290
Momcilo <momcilo.popov@becomes.co>
2023-03-22 18:17:58 +0100

    Make Account settings modal open, when URL modal query contains "account-settings"

--- src/components/settings/account-management.tsx
+++ src/components/settings/account-management.tsx
@@ -47,7 +47,10 @@ const component = defineComponent({
           </div>
         )}
         {/* TODO: Account settings is not a page, but a modal */}
-        <BCMSButton href="https://cloud.thebcms.com/dashboard" newTab={true}>
+        <BCMSButton
+          href="https://cloud.thebcms.com/dashboard?modal=account-settings"
+          newTab={true}
+        >
           {translations.value.page.settings.accountManagement.editCta}
         </BCMSButton>
       </div>
